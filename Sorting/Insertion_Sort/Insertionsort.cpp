e9754358d626fbe6277a9de2f14be9ef889a1d2e
dheerajdrse7en <dheerajdrse7en@gmail.com>
2025-07-27 13:25:58 +0530

    sorting1

---
+++ Sorting/Insertion_Sort/Insertionsort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+void insertion_sort(vector<int> &arr, int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    vector<int> arr;
+    int num;
+    ifstream f("frame_sizes.txt");
+
+    if (f.is_open()) {
+        while (f >> num) arr.push_back(num);
+        f.close();
+    } else {
+        cerr << "Unable to open frame_sizes.txt.\n";
+        return 1;
+    }
+
+    int n = arr.size();
+    clock_t start = clock();
+    insertion_sort(arr, n);
+    clock_t end = clock();
+
+    cout << "Insertion Sort Time Taken (10000 values): "
+         << (double)(end - start) / CLOCKS_PER_SEC << " seconds" << endl;
+
+    ofstream f1("frame_sizes.txt");
+    for (int i = 0; i < n; i++) {
+        f1 << arr[i] << endl;
+    }
+    f1.close();
+
+    return 0;
+}
